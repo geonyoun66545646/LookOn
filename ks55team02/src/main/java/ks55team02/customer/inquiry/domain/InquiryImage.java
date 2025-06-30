80dff0d52eac3f9653fc8a664d298e9008ce8df5
lgs <lgsd3@naver.com>
2025-06-30 17:32:11 +0900

    [lookon-lgs] 스토어 구현 detail : 문의 페이지 구현

---
+++ ks55team02/src/main/java/ks55team02/customer/inquiry/domain/InquiryImage.java
@@ -0,0 +1,12 @@
+package ks55team02.customer.inquiry.domain;
+
+import lombok.Data;
+
+@Data
+public class InquiryImage {
+	
+	private String inqryImgId;
+	private String inqryId;
+	private String imgId;
+	private String ord;
+}
