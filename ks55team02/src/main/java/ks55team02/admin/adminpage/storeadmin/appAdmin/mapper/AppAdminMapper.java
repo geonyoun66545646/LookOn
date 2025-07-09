497a300841bf2454b95e4881dc42ec4f5807276d
lgs <lgsd3@naver.com>
2025-07-09 21:49:07 +0900

    [lookon-lgs] 0709

--- ks55team02/src/main/java/ks55team02/admin/adminpage/storeadmin/appAdmin/mapper/AppAdminMapper.java
+++ ks55team02/src/main/java/ks55team02/admin/adminpage/storeadmin/appAdmin/mapper/AppAdminMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import ks55team02.admin.adminpage.storeadmin.appAdmin.domain.AppAdmin;
+import ks55team02.admin.common.domain.SearchCriteria;
 
 @Mapper
 public interface AppAdminMapper {
@@ -14,5 +15,12 @@ public interface AppAdminMapper {
 	
 	// 신청 상세 아이디 조회
 	AppAdmin getAppAdminById(String aplyId);
+	
+	// AppAdmin 목록의 전체 개수를 가져오는 메서드
+	public int getAppAdminCount(SearchCriteria searchCriteria);
+	
+	// 전체 개수 조회
+	List<AppAdmin> getAppAdminList(AppAdmin appAdmin);
+	int getAppAdminCount(AppAdmin appAdmin); 
 	
 }
