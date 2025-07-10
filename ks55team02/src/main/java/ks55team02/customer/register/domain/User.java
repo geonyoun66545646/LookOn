b6be507543651ac26875c78f20c69b860c10f2b0
aden0918 <nmzxcvb1234@naver.com>
2025-07-10 15:46:03 +0900

    [lookon-ksj] 회원관리 detail : 회원조회 구현

--- ks55team02/src/main/java/ks55team02/customer/register/domain/User.java
+++ ks55team02/src/main/java/ks55team02/customer/register/domain/User.java
@@ -3,7 +3,7 @@ package ks55team02.customer.register.domain;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import ks55team02.systems.crypto.annotation.EncryptedField;
+
 import lombok.Data;
 
 @Data
@@ -12,25 +12,14 @@ public class User {
 	    private String 			userNo;
 	    private String 			mbrGrdCd; // 회원 가입시 기본값: grd_cd_3
 	    private String 			userLgnId;
-	    
-	    @EncryptedField 
-	    private String 			userNm;
-	    
+	    private String 			userNm;    
 	    private String 			userPswdEncptVal; // DB에 저장될 비밀번호 필드
 	    private String 			genderSeCd;
-	    
-	    @EncryptedField
-	    private String 			emlAddr;
-	    
-	    @EncryptedField
-	    private String 			telno;
-	    
+	    private String 			emlAddr; 
+	    private String 			telno;	    
 	    private LocalDate 		userBrdt;
 	    private String 			zipCd;
-	    
-	    @EncryptedField
 	    private String 			addr;
-	    @EncryptedField
 	    private String 			daddr;
 	    private String			userNcnm;
 	    private String			userStatus; 		// 회원 가입시 기본값: 활동
