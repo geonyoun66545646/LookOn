88173ea1806e994e2d50a92b605c89c331ed50b6
lgs <lgsd3@naver.com>
2025-07-25 20:58:45 +0900

    [lookon-lgs] 07/25 어드민 ljs 기능 수정 detail : 어드민 95% 완료

--- ks55team02/src/main/java/ks55team02/admin/adminpage/storeadmin/storemngadmin/controller/StoreMngAdminController.java
+++ ks55team02/src/main/java/ks55team02/admin/adminpage/storeadmin/storemngadmin/controller/StoreMngAdminController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ks55team02.admin.adminpage.storeadmin.storemngadmin.service.StoreMngAdminService;
@@ -76,4 +77,5 @@ public class StoreMngAdminController {
             return new ResponseEntity<>(Map.of("success", false, "message", "상점 상태 변경 중 내부 서버 오류가 발생했습니다."), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    
 }
